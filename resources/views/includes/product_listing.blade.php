f362e9d0f12157997283c2db5babc28d7ed961d0
saurabh smoothgraph <saurabh@smoothgraph.in>
2023-04-26 12:40:41 +0530

    price update display

--- resources/views/includes/product_listing.blade.php
+++ resources/views/includes/product_listing.blade.php
@@ -50,6 +50,7 @@
                                     <a ng-href="<%item.shop.shop_url%>" ng-bind="item.shop.shop_name"></a>
                                </div>
                                <div class="price-wrap" ng-if="item.show_price == 1">
+                                <span class="price-label"> ราคาปัจจุบัน </span>
                                     <div class="price"><%item.weight_per_unit%> <%item.unit_name%>/<%item.package_name%> <br><%item.unit_price%> @lang('common.baht')</div>
                                </div>
                                <div class="price-wrap" ng-if="item.show_price == 0">
