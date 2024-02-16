6d0d3cd57d527541b3a74b93d43f3f89bbf8beeb
saurabh smoothgraph <saurabh@smoothgraph.in>
2024-02-16 11:34:35 +0530

    change of pdf

--- config/app.php
+++ config/app.php
@@ -205,6 +205,7 @@ return [
         /* Email server Configuration to manage from database | End */
 
         Barryvdh\DomPDF\ServiceProvider::class,
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
 
 
     ],
@@ -267,7 +268,7 @@ return [
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'VideoApi'  => Sseffa\VideoApi\Facades\VideoApi::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+        'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class
         // Module zipper allias | Added By Dinesh Kumar Kovid | Start
         //'Zipper' => Chumper\Zipper\Zipper::class
 
